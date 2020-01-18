d82f8c8ac6f990b5111d59af44d2db36c4e61054
Christos <christos.hrousis@gmail.com>
2020-01-18 15:42:22 +1100

    Get basic layout of header going.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import { rhythm, scale } from "../utils/typography"
+
+const Hero = styled.section`
+  align-items: center;
+  background-color: #f9dfcd;
+  display: flex;
+  font-family: "Permanent Marker", cursive;
+  justify-content: center;
+`
+const Bio = styled.div`
+  h1 {
+    font-size: 2.75rem;
+    margin: 0;
+  }
+  h2 {
+    font-size: 1.75rem;
+    margin-top: 0.25rem;
+    margin-bottom: 1.25rem;
+  }
+  p {
+    font-size: 1.25rem;
+    margin: 0;
+  }
+  a {
+    color: #000;
+  }
+`
 
 class Layout extends React.Component {
   render() {
@@ -51,24 +78,80 @@ class Layout extends React.Component {
         </h3>
       )
     }
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    )
+
+    if (location.pathname === rootPath) {
+      return (
+        <div>
+          <Hero>
+            <div>
+              <img src="http://place-puppy.com/200x200" />
+            </div>
+            <Bio>
+              <h1>Christos Hrousis.</h1>
+              <h2>Software Engineer</h2>
+              <p>
+                <a
+                  title="I sometimes tweet, but mostly follow."
+                  href="https://twitter.com/christoshrousis"
+                >
+                  twitter
+                </a>
+                ,{" "}
+                <a
+                  title="Here is an excessive overview of my work history."
+                  href="https://www.linkedin.com/in/christoshrousis"
+                >
+                  linkedin
+                </a>{" "}
+                &amp;{" "}
+                <a
+                  title="If you want to actually send a private message to me."
+                  href="mailto:chris@thinkinpixels.com.au"
+                >
+                  email
+                </a>
+                .
+              </p>
+            </Bio>
+          </Hero>
+          <div
+            style={{
+              marginLeft: `auto`,
+              marginRight: `auto`,
+              maxWidth: rhythm(24),
+              padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            }}
+          >
+            <header>{header}</header>
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+        >
+          <header>{header}</header>
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      )
+    }
   }
 }
 
